475668d612ca84eb233d5134b23c9e8a7f21b5a2
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-12-19 18:08:05 -0300

    Add files via upload

---
+++ JobApplicationAPI/Controllers/PingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobApplicationAPI.Controllers
+{
+    public class PingController : Controller
+    {
+        [HttpGet]
+        [Route("api/ping)")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong!!");
+        }
+    }
+}
